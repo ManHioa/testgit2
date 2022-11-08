331a751825148940d6f6e8a23048f873374850ac
Hoang Hoai Nam <nam.hh@lancsnet.com>
2022-11-08 10:42:06 +0700

    hihi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hihi va hahah 
+          Learn React hihi va hahah sgadcugcgaui
         </a>
       </header>
     </div>
